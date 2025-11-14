60aa62a0953dea43c60e2f38cb56674d4730772c
Hemant tyagi <hemantmu85@gmail.com>
2025-11-14 10:28:55 +0530

    Enhance sidebar info with app functionality description
    Added additional information about app functionality to the sidebar.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import PyPDF2
 st.title("PDF/Text Summarizer")
 st.write("Upload a PDF or paste text to get a concise summary.")
 st.sidebar.info("Developed by Hemant Tyagi")
-st.sidebar.info("GitHub: https://github.com/Hemant-tyagi9",
+st.sidebar.info("GitHub: https://github.com/Hemant-tyagi9"
                 "This app is build to summarize the lengthy chapters to small short notes which saves your time.")
 
 @st.cache_resource
